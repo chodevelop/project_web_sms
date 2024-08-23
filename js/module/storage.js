b9500ea8394d7bade38c1c01a797c0fd28f0884e
chodevelop <cyw5066@naver.com>
2024-08-23 11:02:09 +0900

    update updateRank() for tie score process

--- js/module/storage.js
+++ js/module/storage.js
@@ -40,14 +40,29 @@ class Storage {
         });
     }
 
+    //동점자 처리 알고리즘 구현 완료
     updateRank() {
         this.sortByTotal();
         //순위 갱신 반복문
-        for (let i = 0, rank = 1; i < this.students.length; i++, rank++) {
-            this.students[i].rank = rank;
+
+        //rank - 현재 학생 원소에 부여할 순위, tie = 이전까지의 동석차자 수
+        for (let i = 0, rank = 1, tie = 1; i < this.students.length; i++) {
+            this.students[i].rank = rank;//점수대로 순위 부여
+
+            //지금이 학생의 마지막 배열 원소가 아니고(메모리 오류 방지)
+            if (i < this.students.length - 1) {
+                //현재 학생 원소의 총점이 다음 학생 배열의 총점과 일치한다면
+                if (this.students[i].getTotal() === this.students[i + 1].getTotal()) {
+                    tie += 1;//동점자 수 1 증가 후
+                    continue;//rank 증가 안 시키고 continue(반복문 다시 돌면 동석차 부여됨)
+                }
+            }
+            //동석차 부여가 과정이 끝나면
+            rank += tie;//동석차 수만큼 rank 증가
+            tie = 1;//동석차자 수는 기본값이 1임
         }
-        this.saveLocalStorage();
-    }//동점자 처리 알고리즘 필요
+        this.saveLocalStorage();//rank 부여 결과 localStorage 저장(영속성 부여)
+    }
 
     saveLocalStorage() {
         if (!localStorage) {
@@ -64,10 +79,10 @@ class Storage {
         try {
             this.students = JSON.parse(localStorage.students);
             this.students = this.students.map(student => new Student(
-                parseInt(student.sno), 
-                student.sname, 
-                parseInt(student.korean), 
-                parseInt(student.math), 
+                parseInt(student.sno),
+                student.sname,
+                parseInt(student.korean),
+                parseInt(student.math),
                 parseInt(student.english)));
         } catch (error) {
             this.students = [];
