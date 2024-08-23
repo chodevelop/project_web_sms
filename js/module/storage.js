70d63b9adab62ad793114504d0a271104a66175a
chodevelop <cyw5066@naver.com>
2024-08-23 11:14:13 +0900

    modify updateRank() tie score algorithm

--- js/module/storage.js
+++ js/module/storage.js
@@ -40,28 +40,23 @@ class Storage {
         });
     }
 
-    //동점자 처리 알고리즘 구현 완료
+    //동점자 처리 알고리즘 구현(수정)
     updateRank() {
-        this.sortByTotal();
-        //순위 갱신 반복문
+        this.sortByTotal();//총점 순으로 갱신
+        //rank - 현재 학생 원소에 부여할 순위
+        for (let i = 0, rank = 1; i < this.students.length; i++) {
+            this.students[i].rank = rank;//rank 변수 값대로 순위 부여
 
-        //rank - 현재 학생 원소에 부여할 순위, tie = 이전까지의 동석차자 수
-        for (let i = 0, rank = 1, tie = 1; i < this.students.length; i++) {
-            this.students[i].rank = rank;//점수대로 순위 부여
-
-            //지금이 학생의 마지막 배열 원소가 아니고(메모리 오류 방지)
+            //지금이 학생 배열 마지막 원소가 아니고(메모리 오류 방지)
             if (i < this.students.length - 1) {
                 //현재 학생 원소의 총점이 다음 학생 배열의 총점과 일치한다면
                 if (this.students[i].getTotal() === this.students[i + 1].getTotal()) {
-                    tie += 1;//동점자 수 1 증가 후
-                    continue;//rank 증가 안 시키고 continue(반복문 다시 돌면 동석차 부여됨)
+                    continue;//rank 갱신 없이 continue(반복문 다시 돌면 동석차 부여됨)
                 }
             }
-            //동석차 부여가 과정이 끝나면
-            rank += tie;//동석차 수만큼 rank 증가
-            tie = 1;//동석차자 수는 기본값이 1임
+            rank = i + 2;//동석차 처리 후 rank 값 갱신
         }
-        this.saveLocalStorage();//rank 부여 결과 localStorage 저장(영속성 부여)
+        this.saveLocalStorage();//rank 결과 localStorage 저장(영속성 부여)
     }
 
     saveLocalStorage() {
